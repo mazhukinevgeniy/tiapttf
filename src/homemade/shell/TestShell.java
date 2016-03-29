c1546ab3b0c3445d93b2e417922e66021ce2c8f9
Leon-999 <lionewr@gmail.com>
2016-03-30 01:42:15 +0300

    Start work with xml

--- src/homemade/shell/TestShell.java
+++ src/homemade/shell/TestShell.java
@@ -1,21 +1,30 @@
 package homemade.shell;
 
 import homemade.shell.model.settings.Settings;
+import org.w3c.dom.Document;
 
 import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 
 /**
  * Created by Marid on 27.03.2016.
  */
 public class TestShell
 {
-    public static void main(String [] args)
+    public static void main(String [] args)  throws Exception
     {
         JFrame frame = new JFrame("there's a pattern there to follow");
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         parameterTest();
+
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setValidating(false);
+        DocumentBuilder builder = f.newDocumentBuilder();
+        Document doc = builder.parse(new File("test.xml"));
 
         frame.pack();
         frame.setLocationRelativeTo(null);
