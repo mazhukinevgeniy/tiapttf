e9abbc7c96bed11eb629ab22a303ac18db214bfd
mazhukin.evgeniy <mazhukin.evgeniy@gmail.com>
2016-04-18 12:35:30 +0300

    Assign null where null is assigned anyway

--- src/homemade/game/model/combo/ComboDetector.java
+++ src/homemade/game/model/combo/ComboDetector.java
@@ -139,7 +139,7 @@ public class ComboDetector
             }
             else
             {
-                currentCell = nextCell;
+                currentCell = null;
             }
         }
 
