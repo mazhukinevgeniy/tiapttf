6dba861b2416ef996736c570886c07c9bfc425a8
mazhukin.evgeniy <mazhukin.evgeniy@gmail.com>
2016-04-23 03:25:46 +0300

    Remove obsolete TODO note
    At this moment multiple block selection is not useful for any game mode planned

--- src/homemade/game/view/GameMouseAdapter.java
+++ src/homemade/game/view/GameMouseAdapter.java
@@ -5,9 +5,6 @@ import homemade.game.controller.MouseInputHandler;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-/**
- * Created by user3 on 24.03.2016.
- */
 class GameMouseAdapter extends MouseAdapter
 {
     private MouseInputHandler inputHandler;
@@ -17,12 +14,9 @@ class GameMouseAdapter extends MouseAdapter
         this.inputHandler = inputHandler;
     }
 
-
     @Override
     public void mouseReleased(MouseEvent e)
     {
         this.inputHandler.handleMouseRelease(e.getX(), e.getY());
     }
-
-    //TODO: allow multiple block selection
 }
