73df92a1af847726154d4f8fe27919a48541c006
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-10-07 16:55:35 +0300

    Document more mechanics

--- doc/mechanics.mm
+++ doc/mechanics.mm
@@ -1,6 +1,15 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1475710827221" ID="ID_1054320100" MODIFIED="1475710835290" TEXT="mechanics">
+<node CREATED="1475848374880" ID="ID_439902331" MODIFIED="1475848378402" POSITION="right" TEXT="terms">
+<node CREATED="1475848379734" ID="ID_289725454" MODIFIED="1475848381818" TEXT="combo">
+<node CREATED="1475848382686" ID="ID_937048713" MODIFIED="1475848420091" TEXT="certain amount of blocks in direct or reverse order is a combo"/>
+<node CREATED="1475848420822" ID="ID_767731238" MODIFIED="1475848451730" TEXT="combopack is all simultanious combos"/>
+<node CREATED="1475848452270" ID="ID_169698650" MODIFIED="1475848470569" TEXT="when combopack is scored combo blocks are removed from the field"/>
+<node CREATED="1475848472549" ID="ID_1976563093" MODIFIED="1475848489457" TEXT="dead blocks are destroyed if combo block gets removed in a nearby cell"/>
+</node>
+<node CREATED="1475848513053" ID="ID_1931040274" MODIFIED="1475848516649" TEXT="comboEffects"/>
+</node>
 <node CREATED="1475710856622" ID="ID_1986167927" MODIFIED="1475710860114" POSITION="right" TEXT="scoring">
 <node CREATED="1475711365185" ID="ID_1033335223" MODIFIED="1475711369150" TEXT="comboScore">
 <node CREATED="1475710861662" ID="ID_17024119" MODIFIED="1475710903649" TEXT="comboScore = base * tier * tier"/>
@@ -20,17 +29,23 @@
 </node>
 <node CREATED="1475711656215" ID="ID_370999193" MODIFIED="1475711658699" TEXT="packMult">
 <node CREATED="1475711851582" ID="ID_19546445" MODIFIED="1475711995737" TEXT="aka packTier, it&apos;s a sum of combo tiers"/>
-<node CREATED="1475711997325" ID="ID_1186632827" MODIFIED="1475712030424" TEXT="&#x441;&#x447;&#x435;&#x442; &#x43a;&#x443;&#x431;&#x438;&#x447;&#x435;&#x441;&#x43a;&#x438;&#x439; &#x43e;&#x442; &#x442;&#x438;&#x440;&#x430;!!"/>
+<node CREATED="1475711997325" ID="ID_1186632827" MODIFIED="1475787975515" TEXT="so combo score ~ tier^3"/>
 </node>
 </node>
 </node>
-<node CREATED="1475712616008" ID="ID_938360386" MODIFIED="1475712634619" POSITION="right" TEXT="core rules">
-<node CREATED="1475712635752" ID="ID_576788200" MODIFIED="1475712638875" TEXT="spawn">
-<node CREATED="1475712639608" ID="ID_959591667" MODIFIED="1475712642972" TEXT="overfill">
+<node CREATED="1475712635752" ID="ID_576788200" MODIFIED="1475712638875" POSITION="right" TEXT="spawn">
+<node CREATED="1475712639608" ID="ID_959591667" MODIFIED="1475787630530" TEXT="overfill">
 <node CREATED="1475712643904" ID="ID_1916159058" MODIFIED="1475712665964" TEXT="if you have multiplier &gt; 1, random blocks will be removed and multiplier will be consumed"/>
 <node CREATED="1475712666695" ID="ID_876062660" MODIFIED="1475712674283" TEXT="otherwise you will lose"/>
 </node>
+<node CREATED="1475787632153" ID="ID_762547524" MODIFIED="1475787635638" TEXT="denying">
+<node CREATED="1475787636481" ID="ID_396627455" MODIFIED="1475787739405" TEXT="if you have mult=1 and move a block on top of future spawn, you get dead block in the previous position of moved block"/>
 </node>
+</node>
+<node CREATED="1475787745504" ID="ID_576774394" MODIFIED="1475787747045" POSITION="right" TEXT="blocks">
+<node CREATED="1475787747728" ID="ID_1374051880" MODIFIED="1475787750628" TEXT="dead block"/>
+<node CREATED="1475787750945" ID="ID_377048135" MODIFIED="1475787754108" TEXT="normal block"/>
+<node CREATED="1475787754361" ID="ID_1638519996" MODIFIED="1475787758717" TEXT="immovable block"/>
 </node>
 </node>
 </map>
