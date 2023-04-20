4330731ac19c85c055e5a734b1f10cdbb54021d5
Evgenii Mazhukin <mazhukin.evgeniy@gmail.com>
2023-04-20 16:40:58 +0200

    field serialized successfully

--- game-model-v2/src/homemade/game/state/immutable/PlainFieldState.kt
+++ game-model-v2/src/homemade/game/state/immutable/PlainFieldState.kt
@@ -6,12 +6,22 @@ import homemade.game.fieldstructure.FieldStructure
 import homemade.game.fieldstructure.LinkCode
 import homemade.game.model.CellState
 import homemade.game.state.FieldState
+import homemade.game.state.MutableFieldState
+import homemade.game.state.impl.BlockValuePool
 
-class PlainFieldState(source: FieldState) : FieldState {
-    override val structure: FieldStructure = source.structure
-    val cellStates = structure.cellCodeIterator.asSequence().map { source.getCellState(it) }.toList()
-    val linkState = structure.linkCodeIterator.asSequence().map { source.getLinkBetweenCells(it) }.toList()
-    val lengths = structure.linkCodeIterator.asSequence().map { source.getChainLength(it) }.toList()
+class PlainFieldState(
+        override val structure: FieldStructure,
+        val cellStates: List<CellState>,
+        val linkState: List<Direction?>,
+        val lengths: List<Int>
+) : FieldState {
+
+    constructor(source: FieldState) : this(
+            source.structure,
+            source.structure.cellCodeIterator.asSequence().map { source.getCellState(it) }.toList(),
+            source.structure.linkCodeIterator.asSequence().map { source.getLinkBetweenCells(it) }.toList(),
+            source.structure.linkCodeIterator.asSequence().map { source.getChainLength(it) }.toList()
+    )
 
     override fun getCellState(cellCode: CellCode): CellState {
         check(cellCode.hashCode() in cellStates.indices) {
@@ -32,5 +42,15 @@ class PlainFieldState(source: FieldState) : FieldState {
             "bad link $linkCode in structure ${structure.width}x${structure.height}"
         }
         return lengths[linkCode.hashCode()]
+    }
+
+    companion object {
+        fun buildConsistent(fieldStructure: FieldStructure, cellStates: List<CellState>, maxBlock: Int): PlainFieldState {
+            val tmp = MutableFieldState(fieldStructure, BlockValuePool(maxBlock, fieldStructure.fieldSize))
+            tmp.applyCascadeChanges(fieldStructure.cellCodeIterator.asSequence().associateBy(
+                    { it }, { cellStates[it.hashCode()] })
+            )
+            return PlainFieldState(tmp)
+        }
     }
 }
--- game-serializations/src/homemade/game/model/PlainGameState.kt
+++ game-serializations/src/homemade/game/model/PlainGameState.kt
@@ -1,36 +1,27 @@
 package homemade.game.model
 
 import homemade.game.fieldstructure.CellCode
-import homemade.game.fieldstructure.Direction
 import homemade.game.fieldstructure.FieldStructure
-import homemade.game.fieldstructure.LinkCode
 import homemade.game.state.ConfigState
 import homemade.game.state.FieldState
 import homemade.game.state.GameState
 import homemade.game.state.SelectionState
+import homemade.game.state.immutable.PlainFieldState
 
 internal data class PlainGameState(
         val width: Int,
         val height: Int,
-        val settings: GameSettings
+        val spawnPeriod: Int,
+        val settings: GameSettings,
+        val cellStates: List<CellState>
 ) : GameState() {
 
     @Transient
-    override val fieldState = object : FieldState {
-        override val structure = FieldStructure(width, height)
-
-        override fun getCellState(cellCode: CellCode): CellState {
-            TODO("Not yet implemented")
-        }
-
-        override fun getLinkBetweenCells(linkCode: LinkCode): Direction? {
-            TODO("Not yet implemented")
-        }
-
-        override fun getChainLength(linkCode: LinkCode): Int {
-            TODO("Not yet implemented")
-        }
-    }
+    override val fieldState: FieldState = PlainFieldState.buildConsistent(
+            FieldStructure(width, height),
+            cellStates,
+            settings.maxBlockValue
+    )
 
     @Transient
     override val selectionState = object : SelectionState {
@@ -51,12 +42,18 @@ internal data class PlainGameState(
     }
 
     constructor(source: GameState) : this(
-            source.fieldState.structure.width,
-            source.fieldState.structure.height,
-            source.configState.settings
+            source, PlainFieldState(source.fieldState)
+    )
+
+    private constructor(source: GameState, intermediateField: PlainFieldState) : this(
+            width = source.fieldState.structure.width,
+            height = source.fieldState.structure.height,
+            spawnPeriod = source.currentSpawnPeriod(),
+            settings = source.configState.settings,
+            cellStates = intermediateField.cellStates
     )
 
     override fun currentSpawnPeriod(): Int {
-        TODO("Not yet implemented")
+        return spawnPeriod
     }
 }
