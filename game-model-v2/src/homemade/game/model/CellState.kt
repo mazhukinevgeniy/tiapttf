69aeaf14816aa68963c98254c360829c0fbac8c0
Evgenii Mazhukin <mazhukin.evgeniy@gmail.com>
2023-04-21 04:31:52 +0200

    for-amend

---
+++ game-model-v2/src/homemade/game/model/CellState.kt
@@ -0,0 +1,42 @@
+package homemade.game.model
+
+import homemade.game.model.combo.ComboEffect
+import java.util.*
+
+/**
+ * Extensions of this class must be immutable
+ */
+abstract class CellState protected constructor(private val cellType: Cell) {
+    open fun value(): Int {
+        return UNDEFINED_VALUE
+    }
+
+    open fun effect(): ComboEffect? {
+        return ComboEffect.UNDEFINED_COMBO_EFFECT
+    }
+    //TODO somethings wrong with isMovableBlock and isAliveBlock
+    /**
+     * I think we need this method for view
+     */
+    fun type(): Cell {
+        return cellType
+    }
+
+    val isAliveBlock: Boolean
+        get() =//TODO: find a better method name
+            cellType == Cell.OCCUPIED
+    val isAnyBlock: Boolean
+        get() = blocks.contains(cellType)
+    val isFreeForMove: Boolean
+        get() = cellFreeToMove.contains(cellType)
+    val isFreeForSpawn: Boolean
+        get() = cellType == Cell.EMPTY
+
+    companion object {
+        private val cellFreeToMove: Set<Cell> = EnumSet.of(Cell.EMPTY, Cell.MARKED_FOR_SPAWN)
+        private val blocks: Set<Cell> = EnumSet.of(Cell.OCCUPIED, Cell.DEAD_BLOCK)
+        const val UNDEFINED_VALUE = -1
+        val isMovableBlock = false
+            get() = Companion.field
+    }
+}
--- game-serializations/src/homemade/game/model/PrettyEncoder.kt
+++ game-serializations/src/homemade/game/model/PrettyEncoder.kt
@@ -13,16 +13,53 @@ class PrettyEncoder {
             for (col in 0 until state.fieldState.structure.width) {
                 val cellCode = state.fieldState.structure.getCellCode(col, row)
                 val cellState = state.fieldState.getCellState(cellCode)
-                builder.append(when (cellState.value()) {
-                    CellState.UNDEFINED_VALUE -> "."
-                    else -> cellState.value().toString()
+                builder.append(when (cellState.type()) {
+                    Cell.EMPTY -> "."
+                    Cell.OCCUPIED -> cellState.value().toString()
+                    Cell.DEAD_BLOCK -> "x"
+                    Cell.MARKED_FOR_SPAWN -> "o"
                 })
             }
         }
         return builder.toString()
     }
 
+    /*
+    *
+        val width: Int,
+        val height: Int,
+        val spawnPeriod: Int,
+        val settings: GameSettings,
+        val cellStates: List<CellState>,
+        val selection: Coordinates?,
+        val denies: Int,
+        val score: Int,
+        val multiplier: Int*/
+    /*
+    * val gameMode: GameMode = GameMode.TURN_BASED,
+        val minCombo: Int = 5,
+        val spawn: Int = 4,
+        val period: Int = 500,
+        val maxBlockValue: Int = 9*/
+    /**
+     * format:
+     * [0]selection.x,selection.y or none
+     * [1]spawnPeriod,denies,score,multiplier
+     * [2]mode,minCombo,spawnCount,maxBlockValue // mode = 1 if turn based, 0 if real time
+     * [3+] field in ascii. 1-9 - blocks, . - empty, x - dead
+     */
     fun fromPrettyPrint(pretty: String): GameState {
-        TODO()
+        val lineByLine = pretty.split('\n')
+        check(lineByLine.size >= 4)
+        val selection = run {
+            if (lineByLine[0] == "none") {
+                return@run null
+            }
+            val (x, y) = lineByLine[0].split(',').map { it.toInt() }
+            return@run Coordinates(x, y)
+        }
+        val (spawnPeriod, denies, score, multiplier) = lineByLine[1].split(',').map { it.toInt() }
+        val (mode, minCombo, spawnCount, maxBlockValue) = lineByLine[2].split(',').map { it.toInt() }
+
     }
 }
